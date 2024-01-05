cf08ca1376b6ef38a18d23c10b8024047f5b8294
Sambhaji Shivaji Pandhare <sambhaji.pandhare@in.techwave.net>
2024-01-05 23:03:25 +0530

    auth code added

--- JWT-Authentication-POC/src/main/java/com/jwt/JwtAuthenticationPocApplication.java
+++ JWT-Authentication-POC/src/main/java/com/jwt/JwtAuthenticationPocApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtAuthenticationApplication {
+public class JwtAuthenticationPocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtAuthenticationApplication.class, args);
+		SpringApplication.run(JwtAuthenticationPocApplication.class, args);
 	}
 
 }
--- JWT-Authentication-POC/src/test/java/com/jwt/JwtAuthenticationPocApplicationTests.java
+++ JWT-Authentication-POC/src/test/java/com/jwt/JwtAuthenticationPocApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JwtAuthenticationApplicationTests {
+class JwtAuthenticationPocApplicationTests {
 
 	@Test
 	void contextLoads() {
